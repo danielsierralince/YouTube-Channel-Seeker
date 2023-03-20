acab0eef7c360fbfa349b136ad5d9589426149a4
Daniel Sierra <danielsantiagosierralince@gmail.com>
2023-03-20 00:45:52 -0500

    v0.3.0 released (API)

---
+++ APIconnection.py
@@ -0,0 +1,61 @@
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import os
+
+os.chdir("YouTube Channel Seeker") #os.getcwd()
+
+# Credenciales de la API de YouTube
+# PARA USAR LA API ES NECESARIO TENER CREDENCIALES (CLIENT_SECRET.JSON) Y ACCESO DE DEVELOPER/TESTER. En su defecto, cree su cuenta (https://console.cloud.google.com/), diríjase a Más productos/APIs y servicios/APIs y servicios habilitados/Crear proyecto, diligencie la información, descargue el archivo client_secret.json (client_secret_XXXXCLAVEXXXX.apps.googleusercontent.com.json) y anéxelo a la carpeta actual con el nombre "client_secret.json"
+CLIENT_SECRETS_FILE="client_secret.json" #Clave de la API para poder ingresar (obtenido del proceso de registro)
+SCOPES=["https://www.googleapis.com/auth/youtube.readonly"] #La acción a la que se dirije (en este caso readonly)/Habilita ciertas operaciones
+API_SERVICE_NAME="youtube"
+API_VERSION="v3"
+
+def get_authenticated_service():
+    flow=google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES) #Integra request y abre navegador para autenticarse con cuenta gmail
+    credentials=flow.run_local_server(port=0)
+    return googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials) #Retorna archivo con toda la información
+
+def get_subscriptions():
+    youtube=get_authenticated_service()
+    subscriptions=[]
+    request=youtube.subscriptions().list(
+        part="snippet", #Solicitar llamada a la API. Devuelve información básica (canal, video, lista de reproducción)
+        mine=True,
+        maxResults=500 #Cantidad máxima de resultados/consultas que se permiten hacer
+    ) #Obtener lista de suscripciones
+
+    while request is not None:
+        response=request.execute()
+        for item in response["items"]:
+            title=item["snippet"]["title"]
+            channel_id=item["snippet"]["resourceId"]["channelId"] #Petición al servidor de Google. Extrae información del channelID
+            link=f"https://www.youtube.com/channel/{channel_id}"
+            category = item["snippet"].get("categoryId", "Not available") #Obtiene la categoría y, en caso de no tener, la deja 'Not available'
+            subscription=[title, link, category]
+            subscriptions.append(subscription)
+        request = youtube.subscriptions().list_next(request, response)
+    return subscriptions
+
+def print_subscriptions(subscriptions):
+    for subscription in subscriptions:
+        print(f"Nombre del canal: {subscription['title']}")
+        print(f"Link del canal: {subscription['link']}")
+        print(f"Categoría del canal: {subscription['category']}", end="\n\n")
+
+'''
+# Ejemplo de uso
+subscriptions = get_subscriptions()
+print_subscriptions(subscriptions)
+
+# Ejemplo de uso
+filename = input("Ingrese el nombre del archivo: ")
+try:
+    with open(filename, 'a') as f:
+        subscriptions = get_subscriptions()
+        for subscription in subscriptions:
+            f.write(subscription)
+        print("Información guardada correctamente.")
+except Exception as e:
+    print(f"Error al guardar información en archivo: {e}")
+'''
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # YouTube-Channel-Seeker
-YouTube Channel Seeker is a mini-proyect developed in Python, which implements a graphical interface (GUI) using PyQt5 library. It is a tool that can create, read, update and delete (CRUD) a number of channels with some information such as name, link (URL) and category.
+YouTube Channel Seeker is a mini-proyect developed in Python, which implements a graphical interface (GUI) using PyQt5 library and a YouTube API connection. It's a tool that can create, read, update and delete (CRUD) a number of channels with some information such as name, link (URL) and category, which you can get from channels you're subscribe to.
+
+## Requirements
+
+You can run the following command in the console, after being located in the folder that contains the "requirements.txt" file.
+`pip install -r requirements.txt`
+
+The "requirements.txt" file contains a list of the packages required for the project and their versions. Make sure you have an up-to-date version of the "requirements.txt" file to avoid potential errors.
